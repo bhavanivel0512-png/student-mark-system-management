8dbdaba1b332581de703558ddf2dfce74352b97b
bhavanivel0512 <bhavanivel0512@gamil.com>
2026-05-23 10:37:20 +0530

    deploy redy

--- student-mark/src/components/Dashboard.jsx
+++ student-mark/src/components/Dashboard.jsx
@@ -1,5 +1,4 @@
-import { useEffect, useState } from 'react'
-
+import React, { useEffect, useState } from 'react'
 function Dashboard() {
   const [students, setStudents] = useState([])
   const [loading, setLoading] = useState(true)
